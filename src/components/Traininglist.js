1a6f4e353af398c85d74f566d43e47b5cfe4048e
pineappletea <47184314+pineappletea@users.noreply.github.com>
2020-04-29 21:51:35 +0300

    Update Traininglist.js

--- src/components/Traininglist.js
+++ src/components/Traininglist.js
@@ -2,17 +2,24 @@ import React, {useState, useEffect} from 'react';
 import ReactTable from 'react-table-v6'
 import 'react-table-v6/react-table.css'
 import Moment from 'react-moment';
-
+import Button from '@material-ui/core/Button'
+import Snackbar from '@material-ui/core/Snackbar'
 
 export default function Traininglist() {
     const [trainings, setTrainings] = useState([]);
+    const [msg, setMsg] = useState('');
+    const [open, setOpen] = useState(false);
     var moment = require('moment');
     moment().format();
 
     useEffect(() => {
-        getCustomers();
+        getTrainings();
     }, [])
-    const getCustomers = () => {
+    const handleClose = () =>{
+        setOpen(false);
+
+    }
+    const getTrainings = () => {
         fetch("https://customerrest.herokuapp.com/api/trainings")
         .then (response => response.json())
         .then (
@@ -24,6 +31,26 @@ export default function Traininglist() {
     
     const convertTime = (time) => {
         return moment(time).local().format('MMMM Do YYYY, h:mm:ss a')
+    }
+
+    const deleteTraining = (url) => {
+
+        if (window.confirm("Are you sure?")) {
+
+            fetch(url, {
+                method: 'DELETE',
+                headers: { 'Content-Type': 'application/json' }
+            })
+                .then(_ => {
+                    getTrainings()
+                }
+                )
+                .then(_ => {
+                    setOpen(true)
+                    setMsg('Training deleted');
+                })
+                .catch(err => console.error(err))
+        }
     }
 
 
@@ -40,11 +67,24 @@ export default function Traininglist() {
         },
         { Header: "Duration",
         accessor: "duration" 
+        },
+        {
+            Cell: row => (<Button color="secondary" size="small" onClick={() => deleteTraining(row.original.links[0].href)}>Delete</Button>)
         }
     ]
 
     return (
         <div>
+            <Snackbar
+                open={open}
+                autoHideDuration={2000}
+                onClose={handleClose}
+                message={msg}
+                anchorOrigin={{
+                    vertical: 'top',
+                    horizontal: 'center'
+                }}
+            />
             <ReactTable defaultPageSize={20} filterable={true} data={trainings} columns={columns} />
         </div>
     )
